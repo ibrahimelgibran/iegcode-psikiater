6f967134abdc356a99112e0731f78bc0e21a6d48
ElGibran17 <ibrahimelgibran17@gmail.com>
2024-07-31 22:16:22 +0700

    update

--- app/api/graph/route.ts
+++ app/api/graph/route.ts
@@ -1,12 +1,12 @@
-import { PrismaClient } from '@prisma/client';
-import { NextRequest, NextResponse } from 'next/server';
-import { encryptPassword, decryptPassword } from '@/utils/hash';
+import { PrismaClient } from '@prisma/client'
+import { NextRequest, NextResponse } from 'next/server'
+import { encryptPassword, decryptPassword } from '@/utils/hash'
 
-const prisma = new PrismaClient();
+const prisma = new PrismaClient()
 
 export async function POST(request: NextRequest) {
   // Get the score and userId from the request body
-  const { score, userId } = await request.json();
+  const { score, userId } = await request.json()
 
   // if both score and userId is passed then create the score field
   if (score && userId) {
@@ -15,9 +15,9 @@ export async function POST(request: NextRequest) {
         value: encryptPassword(String(score)),
         userId: userId,
       },
-    });
+    })
 
-    return NextResponse.json(result);
+    return NextResponse.json(result)
   }
 
   // if only userId is passed then get all the score related to the user
@@ -26,17 +26,17 @@ export async function POST(request: NextRequest) {
       where: {
         userId: userId,
       },
-    });
+    })
 
     // Create a new array with decrypted values
-    const decryptedResults = results.map(item => ({
+    const decryptedResults = results.map((item) => ({
       ...item,
       value: Number(decryptPassword(item.value)),
-    }));
+    }))
 
-    return NextResponse.json(decryptedResults);
+    return NextResponse.json(decryptedResults)
   }
 
   // if userId and score is not passed or only score is passed return error
-  return NextResponse.json('error');
+  return NextResponse.json('error')
 }
