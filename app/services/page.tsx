df08c3ef0f24e1f163356237059e4e24d78bbd54
ElGibran17 <ibrahimelgibran17@gmail.com>
2024-08-01 18:44:19 +0700

    update response gemini

--- app/services/page.tsx
+++ app/services/page.tsx
@@ -10,103 +10,112 @@ export default function ServicesPage() {
     <div className="flex flex-wrap gap-10 items-center justify-center my-10">
       <Card className="w-[350px] h-[350px] flex flex-col gap-4 p-5">
         <Image
-          src="/services/home.png"
+          src="/services/homepage.png"
           alt="phq"
           width={400}
           height={400}
           className="cursor-pointer w-full h-52"
         />
-        <p className='font-semibold'>The home of the mental health application</p>
+        <p className="font-semibold">Rumah dari aplikasi kesehatan mental</p>
       </Card>
       <Card className="w-[350px] h-[350px] flex flex-col gap-4 p-5">
         <Image
-          src="/services/auth.png"
+          src="/services/auth-create.png"
           alt="gemini explain"
           width={400}
           height={400}
           className="cursor-pointer w-full h-52"
         />
-        <p className='font-semibold'>Implemented a very robust authentication in the application</p>
+        <p className="font-semibold">
+          Menerapkan otentikasi yang sangat kuat dalam aplikasi
+        </p>
       </Card>
       <Card className="w-[350px] h-[350px] flex flex-col gap-4 p-5">
         <Image
-          src="/services/auth-home.png"
+          src="/services/auth-home1.png"
           alt="gemini message"
           width={400}
           height={200}
           className="cursor-pointer w-full h-52"
         />
-        <p className='font-semibold'>After authentication user can change his data from the User button</p>
+        <p className="font-semibold">
+          Setelah otentikasi, pengguna dapat mengubah datanya dari tombol
+          Pengguna
+        </p>
       </Card>
       <Card className="w-[350px] h-[350px] flex flex-col gap-4 p-5">
         <Image
-          src="/services/about.png"
+          src="/services/aboutus.png"
           alt="personalized response"
           width={400}
           height={400}
           className="cursor-pointer w-full h-52"
         />
-        <p className='font-semibold'>This page tells the details about the project</p>
+        <p className="font-semibold">
+          Halaman ini menceritakan rincian tentang proyek tersebut
+        </p>
       </Card>
       <Card className="w-[350px] h-[350px] flex flex-col gap-4 p-5">
         <Image
-          src="/services/services.png"
-          alt="mental track"
+          src="/services/explain1.png"
+          alt="clerk auth"
           width={400}
           height={400}
           className="cursor-pointer w-full h-52"
         />
-        <p className='font-semibold'>This page shows the services and features of the application</p>
+        <p className="font-semibold">
+          Elemen ini digunakan untuk mengikuti tes dari pengguna untuk melacak
+          depresinya
+        </p>
       </Card>
       <Card className="w-[350px] h-[350px] flex flex-col gap-4 p-5">
         <Image
-          src="/services/contact.png"
+          src="/services/contactus.png"
           alt="complete analysis"
           width={400}
           height={400}
           className="cursor-pointer w-full h-52"
         />
-        <p className='font-semibold'>This form is used to contact in case of emergency</p>
+        <p className="font-semibold">
+          Formulir ini digunakan untuk menghubungi jika terjadi keadaan darurat
+        </p>
       </Card>
       <Card className="w-[350px] h-[350px] flex flex-col gap-4 p-5">
         <Image
-          src="/services/doctor.png"
+          src="/services/doctorr.png"
           alt="contact form"
           width={400}
           height={400}
           className="cursor-pointer w-full h-52"
         />
-        <p className='font-semibold'>This page can only be accessed by professionals who can track all the patients using the application</p>
+        <p className="font-semibold">
+          Halaman ini hanya dapat diakses oleh para profesional yang dapat
+          melacak semua pasien menggunakan aplikasi
+        </p>
       </Card>
       <Card className="w-[350px] h-[350px] flex flex-col gap-4 p-5">
         <Image
-          src="/services/phq.png"
-          alt="chat gemini"
-          width={400}
-          height={400}
-          className="cursor-pointer w-full h-52"
-        />
-        <p className='font-semibold'>This element is used to take the test from the user to track his depression</p>
-      </Card>
-      <Card className="w-[350px] h-[350px] flex flex-col gap-4 p-5">
-        <Image
-          src="/services/gemini.png"
+          src="/services/gemini-ai.png"
           alt="clerk auth"
           width={400}
           height={400}
           className="cursor-pointer w-full h-52"
         />
-        <p className='font-semibold'>You can ask gemini questions from this element</p>
+        <p className="font-semibold">
+          Anda dapat mengajukan pertanyaan gemini dari elemen ini
+        </p>
       </Card>
       <Card className="w-[350px] h-[350px] flex flex-col gap-4 p-5">
         <Image
-          src="/services/explain.png"
-          alt="clerk auth"
+          src="/services/service.png"
+          alt="mental track"
           width={400}
           height={400}
           className="cursor-pointer w-full h-52"
         />
-        <p className='font-semibold'>When you click on explain button a explanation text is shown below gemini element</p>
+        <p className="font-semibold">
+          Halaman ini menunjukkan layanan dan fitur aplikasi
+        </p>
       </Card>
     </div>
   )
